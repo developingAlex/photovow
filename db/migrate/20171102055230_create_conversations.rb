6237b60ac607b7a1ce48b635a4ca3cbf5602adc4
Alex Palma <alex-palma@outlook.com>
2017-11-02 16:59:09 +1100

    Fix conversation migration file

--- db/migrate/20171102055230_create_conversations.rb
+++ db/migrate/20171102055230_create_conversations.rb
@@ -1,10 +1,13 @@
 class CreateConversations < ActiveRecord::Migration[5.1]
   def change
     create_table :conversations do |t|
-      t.references :sender_id, foreign_key: true
-      t.references :recipient_id, foreign_key: true
+      t.references :sender_id, foreign_key: { to_table: :users }
+      t.references :recipient_id, foreign_key: { to_table: :users }
 
       t.timestamps
+
+      add_index :sender_id
+      add_index :recipient_id
     end
   end
 end
