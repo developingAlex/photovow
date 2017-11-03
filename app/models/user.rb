6daf92d6904fc50db568a9086363aadda838f63e
Alex Palma <alex-palma@outlook.com>
2017-11-03 12:50:01 +1100

    Add landing page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def fullname
+    "#{first_name} #{last_name}"
+  end
 end
