5839d9f637f0c45be18e827de38840d541a2aad7
Alex Palma <alex-palma@outlook.com>
2017-11-10 05:06:01 +1100

    Add search functionality to Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,10 @@
 class Listing < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :offers, dependent: :destroy
+  
+  def self.search(search)
+    keyword = search.downcase
+    where("lower(title) LIKE ?", "%#{keyword}%")
+  end
+
 end
